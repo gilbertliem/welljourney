e91af26164d2ad80f7dbda03b5a410630c7a7b69
Gilbert Yohanes <yohanesbert97@gmail.com>
2026-08-16 20:41:04 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/welljourney/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
